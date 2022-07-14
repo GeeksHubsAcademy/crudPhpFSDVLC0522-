334c1169620cefd0e5ccf9c0ebbce6e0a49aaa8f
Dani <d.tarazoma@geekshubs.com>
2022-07-14 13:36:31 +0200

    feat: add form edit task

--- editTaskForm.php
+++ editTaskForm.php
@@ -1,4 +1,5 @@
 <?php
+
     $server = "db";
     $user = 'root';
     $password = 'root';
@@ -6,46 +7,37 @@
 
     try {
         $connection = new PDO("mysql:host=$server;dbname=$databaseName", $user, $password);
+        
+        $idTask = $_GET['id'];
 
-        $sql = "SELECT * FROM tasks";
+        $sql = "SELECT * FROM tasks WHERE (id=$idTask)";
 
         $result = $connection->prepare($sql);
 
         $result->execute();
 
-        $tasks = $result->fetchAll();        
+        $task = $result->fetch(PDO::FETCH_OBJ);
+
+
+        // $result = $connection->pr
     } catch (\Exception $exception) {
         echo 'Error Exception: ' . $exception->getMessage();
-    }
+    }   
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    <h1>Crear Tarea:</h1>
-    <form action="createTask.php" method="post">
-        <p>Title: <input type="text" name="title" /></p>
-        <p><input type="submit" value="Enviar"/></p>
+    <form action="updateTask.php" method="post">
+        <input type="text" name="title" value="<?php echo $task->title; ?>">
+        <input type="submit" value="Actualizar tareas" name="submit">
     </form>
-
-    <h1>Listado de tareas</h1>
-    <ul>
-        <?php foreach($tasks as $task): ?>
-            <li>
-                <button><a href="./editTaskForm.php?id=<?php echo $task['id']; ?>">Edit task</a></button>
-                <?php echo $task['id']; ?> - <?php echo $task['title']; ?>
-            </li>
-        <?php endforeach;?>
-
-    </ul>
 </body>
-
 </html>
